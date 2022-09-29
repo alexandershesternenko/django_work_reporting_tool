57969eee1da401ed7946618f7dad7307c783e4af
alexandershesternenko <alexander.shesternenko@gmail.com>
2022-09-29 16:34:36 +0300

    update

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path, include
 from users import views
-from users.views import signup, change_password
-from django.contrib.auth import views as auth_views
-
+from users.views import signup
 
 urlpatterns = [
 

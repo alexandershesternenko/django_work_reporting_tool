87e0c66f660d825442bda741bd182fa81cc59c0e
alexandershesternenko <alexander.shesternenko@gmail.com>
2022-10-02 13:05:08 +0300

    update design

--- djangoReportingTool/settings.py
+++ djangoReportingTool/settings.py
@@ -43,7 +43,11 @@ INSTALLED_APPS = [
     'django_filters',
     'ReportingTool',
     'users',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
